bca8a9b841f257228d4a40d4aaba782b77a91ec8
alexis <alexis@hellomybusiness.fr>
2020-10-01 21:37:49 +0200

    tout ok reste les views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create!(:name=>'Praline', :address=>'Paris', :phone_number=>'064682919', :category=>'french')
+Restaurant.create!(:name=>'Fitju', :address=>'Vietnam', :phone_number=>'000211234682919', :category=>'japanese')
+Restaurant.create!(:name=>'Big fernand', :address=>'Nice', :phone_number=>'034682919', :category=>'italian')
+Restaurant.create!(:name=>'Nogokiri', :address=>'Pékin', :phone_number=>'unknow', :category=>'italian')
